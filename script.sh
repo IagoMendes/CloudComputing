9725ab1c5d609589dd29293172ca2d6cdf17a1dc
IagoMendes <iagorainhamendes@hotmail.com>
2019-11-07 14:53:31 -0300

    final script

--- script.sh
+++ script.sh
@@ -4,4 +4,6 @@ sudo apt install python3-pip -y
 sudo pip3 install fastapi
 sudo pip3 install uvicorn
 sudo pip3 install email-validator
+git clone https://github.com/IagoMendes/CloudComputing.git
+cd CloudComputing/APS1
 uvicorn main:app --reload --host 0.0.0.0
